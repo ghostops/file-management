4504a1b7908ff12eeef76af587c128ab92a1fe4e
ghostops <ludvig@ghostops.nu>
2016-06-24 12:53:47 +0200

    Test index created.

--- file-management.php
+++ file-management.php
@@ -181,19 +181,4 @@ class FileManagement
 		return TRUE;
 	}
 }
-echo "<pre style='height:200px;overflow:scroll;'>";
-$FM = new FileManagement("./");
-print_r($FM->ReturnFiles());
-echo "</pre>";
 
-if($FM->DeleteFile("404.html"))
-	echo "True";
-
-
-$FM->DeleteFolder("doc/");
-?>
-<form action="file-management.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="test">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php require 'file-management.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Testing</title>
+</head>
+<body>
+	<?php 
+	echo "<pre style='height:200px;overflow:scroll;'>";
+	$FM = new FileManagement("./");
+	print_r($FM->ReturnFiles());
+	echo "</pre>";
+
+	if($FM->DeleteFile("404.html"))
+		echo "True";
+
+
+	$FM->DeleteFolder("doc/");
+	?>
+
+	<form action="file-management.php" method="post" enctype="multipart/form-data">
+	    Select image to upload:
+	    <input type="file" name="test">
+	    <input type="submit" value="Upload Image" name="submit">
+	</form>
+</body>
+</html>
